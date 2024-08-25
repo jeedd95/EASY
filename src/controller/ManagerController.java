db2a7448073b2eb9d246898f1837e8c40153de0f
JHW <jeedd95@gmail.com>
2024-08-25 15:56:04 +0900

    style(관리자) : 메소드 틀만 생성

--- src/controller/ManagerController.java
+++ src/controller/ManagerController.java
@@ -1,5 +1,20 @@
 package controller;
 
-public class ManagerController {
+import java.util.List;
 
+import model.service.MemberService;
+import model.vo.MemberVO;
+
+public class ManagerController {
+	MemberService service = null; // impl 추가
+	
+	public List<MemberVO> searchAllMember(){
+		
+		return null;
+	};
+	
+	public MemberVO searchMemeberById(String memberId) {
+		
+		return null;
+	}
 }
