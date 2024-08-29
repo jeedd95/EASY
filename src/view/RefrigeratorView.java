369d265d258e056c272bce5b0ffeeb31f8d26fe5
jwlee <jwlee1130@naver.com>
2024-08-29 11:51:09 +0900

    ui 추가

--- src/view/RefrigeratorView.java
+++ src/view/RefrigeratorView.java
@@ -37,21 +37,23 @@ public class RefrigeratorView {
 		
 		System.out.println();
 		System.out.println();
-		map[0] = new StringBuffer("                 E.A.S.Y :" + member.getMNickname());
+		map[0] = new StringBuffer("            E.A.S.Y :" +member.getMNickname());
 		map[1] = new StringBuffer("  ┌───────────────────────────────────────────┐");
-		map[2] = new StringBuffer("  │ 식료품 | 수량                                │");
+		map[2] = new StringBuffer("    식료품 | 수량                                 ");
 		map[3] = new StringBuffer("  ├───────────────────────────────────────────┤");
-		for (int i = 4; i < 22; i++) {
-			if (i  < 13)
-				map[i] = new StringBuffer( (i-3) + " │                     │                     │"); // 번호 있는 줄
+		for(int i = 4; i < 22; i++) {
+			if(i%2 != 0)
+				map[i]=new StringBuffer("  ├───────────────────────────────────────────┤");
 			else
-				map[i] = new StringBuffer( (i-3) + "│                     │                     │"); // 번호 없는 줄
-
-			map[16] = new StringBuffer("13├─────────────────────┼─────────────────────┤");
-			map[22] = new StringBuffer("19│                     │                     │");
-			map[23] = new StringBuffer("  └─────────────────────┴─────────────────────┘");
-		}
-
+				{if(i/2<10)
+					map[i]=new StringBuffer((i/2)+"                                              ");
+				 else
+					 map[i]=new StringBuffer((i/2)+"                                             ");}
+			map[22]=new StringBuffer("11│                                           │");
+			map[23]=new StringBuffer("  └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘");
+		}	
+	
+	
 		for (int i = 0; i < refriStatus.size(); i++) { // 냉장고 현황(보유 품목) 개수만큼 반복
 			RefrigeratorVO refre = refriStatus.get(i); // 냉장고 현황 한줄씩 꺼내기
 
@@ -62,7 +64,7 @@ public class RefrigeratorView {
 			// 식자재 이름 char 타입으로 꺼내 set으로 대입 (char[] > char)
 			for (int j = 0; j < ingredientChar.length; j++) { // 식자재 이름 char[] 길이만큼 반복
 				char ingredientCh = ingredientChar[j]; // char타입 하나씩 꺼내서
-				map[i + 4].setCharAt(j + 4, ingredientCh); // set으로 대입하기
+				map[4+2*i].setCharAt(j + 4, ingredientCh); // set으로 대입하기
 			}
 
 			// 냉장고 현황에서 식자재 수량 꺼내서 char[]로 변환 (int > String > char[])
@@ -73,8 +75,9 @@ public class RefrigeratorView {
 			// 식자재 수량 char타입으로 변환해 출력 (char[] > char)
 			for (int k = 0; k < amountCharArr.length; k++) {
 				char amountCh = amountCharArr[k];
-				map[i + 4].setCharAt(k + 10, amountCh);
+				map[4+2*i].setCharAt(k + 10, amountCh);
 			}
+			
 
 		}
 		for (int i = 0; i < 24; i++)
@@ -84,25 +87,3 @@ public class RefrigeratorView {
 
 }
 
-// 이전코드 복사본
-//	public void DrawMap(MemberVO member) {
-//		map[0]=new StringBuffer("            E.A.S.Y :" +member.getMNickname());
-//		map[1]=new StringBuffer("  ┌───────────────────────────────────────────┐");
-//		for(int i = 2; i < 22; i++) {
-//			if(i%2 != 0)
-//				map[i]=new StringBuffer("  ├───────────────────────────────────────────┤");
-//			else
-//				{if(i/2<10)
-//					map[i]=new StringBuffer((i/2)+" │   │   │   │   │   │   │   │   │   │   │   │");
-//				 else
-//					 map[i]=new StringBuffer((i/2)+"│   │   │   │   │   │   │   │   │   │   │   │");}
-//			map[22]=new StringBuffer("11│   │   │   │   │   │   │   │   │   │   │   │");
-//			map[23]=new StringBuffer("  └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘");
-//		}	
-//		map[2].setCharAt(3, 'a');
-//		map[2].setCharAt(4, 'a');
-//		map[2].setCharAt(5, 'a');
-//		map[2].setCharAt(6, 'a');
-//		for(int i=0; i<24;i++)
-//			System.out.println(map[i]);
-//	}
